7890fc2829423311badff8f8bb6701b8c1ecd9c2
Tomas Nania <tnania2022@gmail.com>
2024-09-15 16:49:48 -0300

    estilos

--- detalle.php
+++ detalle.php
@@ -12,19 +12,37 @@ if (file_exists($pokemons_file)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="detallePokemones.css">
+    <link rel="stylesheet" href="global.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <title>Detalle de Pokémon</title>
 </head>
-<body>
+<body >
+<header>
+    <img src="img/image.png">
+    <h1>Pokedex</h1>
+    <button>Iniciar sesion</button>
+
+</header>
 <?php
 if (isset($_GET['id'])) {
     $pokemon_id = $_GET['id'];
     foreach ($pokemons as $pokemon) {
         if ($pokemon['id'] == $pokemon_id) {
             ?>
-            <h2><?php echo ucfirst($pokemon['name']); ?></h2>
-            <img src="<?php echo $pokemon['img']; ?>" alt="<?php echo ucfirst($pokemon['name']); ?>">
-            <h3>Tipo: <?php echo ucfirst($pokemon['type']); ?></h3>
-            <p><?php echo $pokemon['description']; ?></p>
+            <div class="container " >
+                <div class="row">
+                    <div class="col col-6">
+                        <h2><?php echo $pokemon['name']; ?></h2>
+                        <img src="<?php echo $pokemon['img']; ?>" alt="<?php echo $pokemon['name']; ?>">
+                        <h3>Tipo: <?php echo $pokemon['type']; ?></h3>
+                    </div>
+                    <div class="col col-6">
+                        <p style="text-align: left"><?php echo $pokemon['description']; ?></p>
+                    </div>
+                </div>
+            </div>
             <?php
             break;
         }
--- lista.php
+++ lista.php
@@ -12,9 +12,18 @@ if (file_exists($pokemons_file)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="global.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <title>Lista de Pokémon</title>
 </head>
 <body>
+<header>
+    <img src="img/image.png">
+    <h1>Pokedex</h1>
+    <button>Iniciar sesion</button>
+
+</header>
 <h1>Lista de Pokémon</h1>
 <ul>
     <?php foreach ($pokemons as $pokemon): ?>
