89e2af242bd645b420e914f3891f4ffdaa76e584
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2024-09-23 14:43:11 -0300

    diseño detalle pokemon done

---
+++ components/header.php
@@ -0,0 +1,18 @@
+<header class="col-12">
+    <div class="col-10 mx-auto d-flex flex-wrap justify-content-between align-items-center bor1">
+        <div class="h-75 rounded-circle overflow-hidden">
+            <img src="img/image.png" alt="Logo" class="h-100">
+        </div>
+        <a href="/pokedex/index.php" class="text-decoration-none">
+            <h1 class="text-white">Pokedex</h1>
+        </a>
+        <div class="d-flex gap-2 flex-wrap">
+            <a href="/pokedex/login.php">
+                <button>Iniciar sesion</button>
+            </a>
+            <a href="/pokedex/registro.php">
+                <button>Registrarse</button>
+            </a>
+        </div>
+    </div>
+</header>
--- detalle.php
+++ detalle.php
@@ -142,101 +142,46 @@ $pokemones = [
     ]
 ];
 
-$resultado = "";
-$miBusquedad = [];
-$acertados = 0;
-
-$quiereBuscar = isset($_GET["param"]) ? $_GET["param"] : false;
-if ($quiereBuscar) {
-    $resultado = "BUSCANDO";
-    foreach ($pokemones as $poke) { // hola, ola -> true
-        if (str_contains(strtolower($poke["nombre"]), strtolower($quiereBuscar)) !== false) {
-            $miBusquedad[] = $poke;
-            $acertados++;
-        }
-    }
-    $resultado = "Resultados encontrados: " . count($miBusquedad);
-} else {
-    $resultado = "NO BUSCANDO";
+$existeBuscado = isset($_GET["id"]) ? $_GET["id"] : false;
+$pokemon_id = $existeBuscado ?: null;
+$pokemonBuscado = null;
+foreach ($pokemones as $pokemon) {
+    if ($pokemon['codigo'] == $pokemon_id) $pokemonBuscado = $pokemon;
 }
-
 
 ?>
 
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include ("./components/bootstrap-and-general-styles.html")?>
     <!--  Estilos del index  -->
-    <link rel="stylesheet" href="stylesheets/index.css">
-    <title>Pokedex</title>
+    <link rel="stylesheet" href="/pokedex/stylesheets/detalle.css">
+    <title>Detalle de Pokémon</title>
 </head>
 <body>
 <?php require ("./components/header.php")?>
-<main class="col-12 pb-5">
-    <div class="col-5 mx-auto pt-2">
-        <form class="col-12 d-flex flex-wrap">
-            <a href="index.php" class="styledCleanButton col-2">
-                Limpiar
-            </a>
-            <input name="param" class="col-8 styledSearchInput" type="text"
-                   placeholder="Ingrese el nombre, tipo o número de pokemon...">
-            <input class="col-2 styledSearchButton" type="submit" value="Buscar">
-        </form>
-    </div>
-    <!--  TEXTO RESULTADOS  -->
-    <?php if ($quiereBuscar !== false): ?>
-        <div class="mx-auto col-12 text-center">
-            <p class="text-white m-0 p-0"> Se encontraron <?= count($miBusquedad) ?> resultados coincidentes</p>
+<main class="col-12 d-flex justify-content-center align-items-center">
+    <!--  WRAPPER  -->
+    <div class="col-4 mx-auto d-flex flex-column gap-2 p-2 pokemonCard">
+        <div class="pokemonImageContainer">
+            <img src="<?= $pokemonBuscado['imagen']; ?>" alt="<?= ucfirst($pokemonBuscado['nombre']); ?>">
         </div>
-    <?php endif; ?>
-    <!-- WRAPPER -->
-    <div class="col-10 mx-auto pt-4">
-        <!-- nombre, codigo, descripcion -->
-        <div class="pokemonesContainer">
-            <!-- MAP -->
-            <?php if ($quiereBuscar !== false): ?>
-                <?php foreach ($miBusquedad as $pokemon): ?>
-                    <div class="bg-white p-2 pokemonCard">
-                        <div class="pokemonImageContainer">
-                            <img src="<?= $pokemon["imagen"] ?>" class="w-100"/>
-                        </div>
-                        <div class="d-flex flex-column align-items-center justify-content-center gap-1 pokemonTextContainer p-1">
-                            <p class="m-0 p-0 fw-bold"><?= $pokemon["codigo"] ?></p>
-                            <p class="m-0 p-0 fs-6"><?= $pokemon["nombre"] ?></p>
-                            <button>Ver detalles</button>
-                            <!--  <p class="m-0 p-0 fs-6">
-                            Descripción: -->
-                            <?php //= $pokemon["descripcion"] ?>
-                            <!--</p>-->
-                        </div>
-                    </div>
+        <div class="d-flex flex-column gap-2 px-4 pb-4">
+            <h2 class="text-center text-white"><?= ucfirst($pokemonBuscado['nombre']); ?></h2>
+            <p class="m-0 fs-5 text-white">Tipos</p>
+            <div class="d-flex gap-2 flex-wrap">
+                <?php foreach ($pokemonBuscado["tipos"] as $tipo) :?>
+                <div class="btn btn-info">
+                    <p class="m-0"><?= $tipo ?></p>
+                </div>
                 <?php endforeach; ?>
-            <?php else: ?>
-                <?php foreach ($pokemones as $pokemon): ?>
-                    <div class="bg-white p-2 pokemonCard">
-                        <div class="pokemonImageContainer">
-                            <img src="<?= $pokemon["imagen"] ?>" class="w-100"/>
-                        </div>
-                        <div class="d-flex flex-column align-items-center justify-content-center gap-1 pokemonTextContainer p-1">
-                            <p class="m-0 p-0 fw-bold"><?= $pokemon["codigo"] ?></p>
-                            <p class="m-0 p-0 fs-6"><?= $pokemon["nombre"] ?></p>
-                            <a href="detalle.php?id=<?= $pokemon["codigo"] ?>">
-                                <button>Ver detalles</button>
-                            </a>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-            <?php endif; ?>
+            </div>
+            <p class="m-0 text-white"><?= $pokemonBuscado['descripcion']; ?></p>
         </div>
     </div>
 </main>
-<?php include_once("./components/footer.php") ?>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
 </body>
 </html>
