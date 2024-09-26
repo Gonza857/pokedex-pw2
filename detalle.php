9fcd7cc03ee2cd774d5e4e82ce725ddfd9467efd
SolAriass <ssolariaas@gmail.com>
2024-09-26 17:57:28 -0300

    Se agregaron imagenes de todos los tipos de pokemones, se hicieron modificaciones en la base de datos agregando una columna de url y se modifico el detalle para poder visualizar el tipo como una imagen

--- detalle.php
+++ detalle.php
@@ -1,153 +1,32 @@
 <?php
-$pokemones = [
-    [
-        "codigo" => "1",
-        "nombre" => "Bulbasaur",
-        "descripcion" => "Un Pokémon planta y veneno. Es conocido por la semilla en su espalda.",
-        "tipos" => ["Planta", "Veneno"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "2",
-        "nombre" => "Ivysaur",
-        "descripcion" => "La evolución de Bulbasaur. Tiene una flor en su espalda.",
-        "tipos" => ["Planta", "Veneno"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "3",
-        "nombre" => "Venusaur",
-        "descripcion" => "La evolución final de Bulbasaur. Tiene una flor grande en su espalda.",
-        "tipos" => ["Planta", "Veneno"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "4",
-        "nombre" => "Charmander",
-        "descripcion" => "Un Pokémon de tipo fuego. Su cola arde con una llama.",
-        "tipos" => ["Fuego"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "5",
-        "nombre" => "Charmeleon",
-        "descripcion" => "La evolución de Charmander. Su temperamento se vuelve más agresivo.",
-        "tipos" => ["Fuego"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "6",
-        "nombre" => "Charizard",
-        "descripcion" => "La evolución final de Charmander. Un dragón que vuela y lanza fuego.",
-        "tipos" => ["Fuego", "Volador"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "7",
-        "nombre" => "Squirtle",
-        "descripcion" => "Un Pokémon de tipo agua con una concha dura que lo protege.",
-        "tipos" => ["Agua"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "8",
-        "nombre" => "Wartortle",
-        "descripcion" => "La evolución de Squirtle. Su cola es más larga y peluda.",
-        "tipos" => ["Agua"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "9",
-        "nombre" => "Blastoise",
-        "descripcion" => "La evolución final de Squirtle. Tiene cañones de agua en su espalda.",
-        "tipos" => ["Agua"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "10",
-        "nombre" => "Caterpie",
-        "descripcion" => "Un Pokémon oruga que evoluciona rápidamente.",
-        "tipos" => ["Bicho"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "11",
-        "nombre" => "Metapod",
-        "descripcion" => "La evolución de Caterpie. Se encierra en un capullo para protegerse.",
-        "tipos" => ["Bicho"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "12",
-        "nombre" => "Butterfree",
-        "descripcion" => "La evolución final de Caterpie. Un Pokémon mariposa que puede volar.",
-        "tipos" => ["Bicho", "Volador"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "13",
-        "nombre" => "Weedle",
-        "descripcion" => "Un Pokémon insecto con un aguijón venenoso en la cabeza.",
-        "tipos" => ["Bicho", "Veneno"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "14",
-        "nombre" => "Kakuna",
-        "descripcion" => "La evolución de Weedle. En su estado de capullo, apenas se mueve.",
-        "tipos" => ["Bicho", "Veneno"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "15",
-        "nombre" => "Beedrill",
-        "descripcion" => "La evolución final de Weedle. Tiene grandes aguijones en sus patas.",
-        "tipos" => ["Bicho", "Veneno"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "16",
-        "nombre" => "Pidgey",
-        "descripcion" => "Un pequeño Pokémon volador que es muy común en las zonas rurales.",
-        "tipos" => ["Normal", "Volador"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "17",
-        "nombre" => "Pidgeotto",
-        "descripcion" => "La evolución de Pidgey. Es más rápido y fuerte.",
-        "tipos" => ["Normal", "Volador"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "18",
-        "nombre" => "Pidgeot",
-        "descripcion" => "La evolución final de Pidgey. Sus alas le permiten volar a grandes velocidades.",
-        "tipos" => ["Normal", "Volador"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "19",
-        "nombre" => "Rattata",
-        "descripcion" => "Un pequeño roedor que tiene dientes afilados.",
-        "tipos" => ["Normal"],
-        "imagen" => "img/image.png"
-    ],
-    [
-        "codigo" => "20",
-        "nombre" => "Raticate",
-        "descripcion" => "La evolución de Rattata. Tiene colmillos grandes y peligrosos.",
-        "tipos" => ["Normal"],
-        "imagen" => "img/image.png"
-    ]
-];
+$conexion = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "pokedex") or die ("error en conexion");
 
 $existeBuscado = isset($_GET["id"]) ? $_GET["id"] : false;
-$pokemon_id = $existeBuscado ?: null;
-$pokemonBuscado = null;
-foreach ($pokemones as $pokemon) {
-    if ($pokemon['codigo'] == $pokemon_id) $pokemonBuscado = $pokemon;
+$query = "SELECT *
+          FROM pokemon p 
+          JOIN tipo t ON p.TIPO_POKEMON = t.ID 
+          WHERE p.ID_BASE = '$existeBuscado'";
+
+$resultado = mysqli_query($conexion, $query);
+
+
+if(mysqli_num_rows($resultado) == 0){
+    header("Location: index.php");
+    exit();
 }
+
+
+$resultadoConvertido = mysqli_fetch_assoc($resultado);
+
+$imagen = 'imagenes-pokemon/' . $resultadoConvertido["IMAGEN"];
+
+
+$tipo = 'tipos/' . $resultadoConvertido["imagen"];
+
 
 ?>
 
@@ -165,21 +44,17 @@ foreach ($pokemones as $pokemon) {
 <?php require ("./components/header.php")?>
 <main class="col-12 d-flex justify-content-center align-items-center">
     <!--  WRAPPER  -->
-    <div class="col-4 mx-auto d-flex flex-column gap-2 p-2 pokemonCard">
+    <div class="col-4 mx-auto d-flex flex-column gap-2 p-2 justify-content-center align-items-center pokemonCard">
         <div class="pokemonImageContainer">
-            <img src="<?= $pokemonBuscado['imagen']; ?>" alt="<?= ucfirst($pokemonBuscado['nombre']); ?>">
+            <img class="w-50" src="<?= $imagen; ?>" alt="<?= ucfirst($resultadoConvertido['NOMBRE']); ?>">
         </div>
         <div class="d-flex flex-column gap-2 px-4 pb-4">
-            <h2 class="text-center text-white"><?= ucfirst($pokemonBuscado['nombre']); ?></h2>
-            <p class="m-0 fs-5 text-white">Tipos</p>
+            <h2 class="text-center text-white"><?= ucfirst($resultadoConvertido['NOMBRE']); ?></h2>
+            <p class="m-0 fs-5 text-white">Tipo</p>
             <div class="d-flex gap-2 flex-wrap">
-                <?php foreach ($pokemonBuscado["tipos"] as $tipo) :?>
-                <div class="btn btn-info">
-                    <p class="m-0"><?= $tipo ?></p>
-                </div>
-                <?php endforeach; ?>
+                    <img src="<?= $tipo  ?>" class="w-25 m-0 rounded-circle"   alt="<?= ucfirst($resultadoConvertido['NOMBRE']); ?>">
             </div>
-            <p class="m-0 text-white"><?= $pokemonBuscado['descripcion']; ?></p>
+            <p class="m-0 text-white"><?= $resultadoConvertido['DESCRIPCION']; ?></p>
         </div>
     </div>
 </main>
