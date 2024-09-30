64792c5c3eef0ec16278ce7ea7297959deb43aff
SolAriass <ssolariaas@gmail.com>
2024-09-29 22:52:30 -0300

    colores en el resultado-accion

--- resultado-accion.php
+++ resultado-accion.php
@@ -10,5 +10,19 @@ unset($_SESSION["accion-mensaje"]);
 
 ?>
 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="stylesheets/resultadoAccion.css">
+</head>
+<body>
 <h1><?= $mensajeAgregado ?></h1>
 <a href="admin.php">Volver a admin</a>
+</body>
+</html>
+
+
+
